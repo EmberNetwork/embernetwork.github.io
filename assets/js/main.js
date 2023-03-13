77870c9af667cd85cc0d4b3af31effe4bbdea2f2
Russell2259 <russell2259@tuta.io>
2023-03-13 15:07:30 -0600

    try and fix themes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    alert('dark');
 } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    alert('light');
 }
